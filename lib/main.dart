d965306c00ec4f643f73ff093a9b1b6ffa3b07d3
David Suge <63927686+sugedavid@users.noreply.github.com>
2024-05-02 12:23:10 +0100

    chore: firebase migration (#24)

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
+import 'package:banking_app/firebase_options.dart';
 import 'package:banking_app/models/user.dart';
 import 'package:banking_app/shared/main_scaffold.dart';
 import 'package:banking_app/utils/colors.dart';
 import 'package:banking_app/utils/firebase_utils/user_utils.dart';
 import 'package:banking_app/views/email_verification/email_verification_page.dart';
 import 'package:banking_app/views/login/login_page.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.appAttest,
   );
   runApp(const MyApp());
 }
