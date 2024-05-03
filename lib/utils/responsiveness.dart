1efae2c4afccd3c7813b1e87bf587f1c73ade5d5
David Suge <63927686+sugedavid@users.noreply.github.com>
2024-05-03 22:20:35 +0100

    chore: update ci 2 (#28)

--- lib/utils/responsiveness.dart
+++ lib/utils/responsiveness.dart
@@ -1,6 +1,6 @@
-// large screen
 import 'package:flutter/material.dart';
 
+// large screen
 bool isLargeScreen(BuildContext context) {
   final screenSizeInches = MediaQuery.of(context).size.shortestSide /
       MediaQuery.of(context).devicePixelRatio;
