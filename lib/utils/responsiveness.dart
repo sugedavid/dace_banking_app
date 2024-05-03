5a4e55a039c34bd6be2e513b4fc34fbda3b82fed
David Suge <63927686+sugedavid@users.noreply.github.com>
2024-05-03 21:03:50 +0100

    chore: update ci

--- lib/utils/responsiveness.dart
+++ lib/utils/responsiveness.dart
@@ -4,11 +4,5 @@ import 'package:flutter/material.dart';
 bool isLargeScreen(BuildContext context) {
   final screenSizeInches = MediaQuery.of(context).size.shortestSide /
       MediaQuery.of(context).devicePixelRatio;
-
-  // Check if the screen size is greater than 7 inches
-  if (screenSizeInches > 7) {
-    return true;
-  } else {
-    return false;
-  }
+  return screenSizeInches > 200;
 }
